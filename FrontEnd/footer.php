7ee49c2e68b76b3a3e5e03edd32753e35a45321e
Sumaiya Islam Mim <2110903@iub.edu.bd>
2025-08-21 02:05:05 +0600

    Update footer.php

--- FrontEnd/footer.php
+++ FrontEnd/footer.php
@@ -2,9 +2,10 @@
     <link rel="stylesheet" href="footer.css">
     <div class="container">
         <p>
-            © Copyright 2024 <a href="https://www.facebook.com/TESL.EnglishVersionSchoolandCollege/" target="_blank"> TESL </a> & 
-            <a href="https://www.facebook.com/share/16Q4cEdSzy/" target="_blank"> Md Shahduzzaman </a>. 
+            © Copyright 2024 <a href="https://www.facebook.com/TESL.EnglishVersionSchoolandCollege/" target="_blank"> XYZ </a> & 
+            <a href="www.linkedin.com/in/sumaiya-islam-mim-45400b344" target="_blank"> Sumaiya Islam Mim </a>. 
             All rights reserved.
         </p>
     </div>
-</div>
+
+ </div>
